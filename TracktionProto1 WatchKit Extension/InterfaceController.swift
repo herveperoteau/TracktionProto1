ab8b0b5c1fed984f0375d19ba5582a795a00e555
Herve Peroteau <herve.peroteau@gmail.com>
2015-12-29 17:14:47 +0100

    First setup project

---
+++ TracktionProto1 WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TracktionProto1 WatchKit Extension
+//
+//  Created by Hervé PEROTEAU on 29/12/2015.
+//  Copyright © 2015 Hervé PEROTEAU. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ TracktionProto1Tests/TracktionProto1Tests.swift
@@ -0,0 +1,36 @@
+//
+//  TracktionProto1Tests.swift
+//  TracktionProto1Tests
+//
+//  Created by Hervé PEROTEAU on 29/12/2015.
+//  Copyright © 2015 Hervé PEROTEAU. All rights reserved.
+//
+
+import XCTest
+@testable import TracktionProto1
+
+class TracktionProto1Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
